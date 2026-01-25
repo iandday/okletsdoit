42994adee2e12d2f290e7478c9d6964dcbd203f7
Ian Day <ian@ianday.me>
2026-01-25 16:18:43 -0500

    fix(Core): :bug: fix venue preview

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,9 +1,14 @@
 import { getconfigData } from "$lib/server/config-data";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async () => {
     const configData = await getconfigData();
 
+    // redirect to home if no config data
+    if (!configData) {
+        throw redirect(302, "/");
+    }
     return {
         configData,
     };
