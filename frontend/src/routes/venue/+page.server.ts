fa07a13e3205a27aa6e2405310d22523eb3bd5c2
Ian Day <ian@ianday.me>
2026-07-12 12:18:36 -0400

    feat(Core): :zap: anble/disable venue

--- frontend/src/routes/venue/+page.server.ts
+++ frontend/src/routes/venue/+page.server.ts
@@ -1,13 +1,21 @@
+// frontend/src/routes/venue/+page.server.ts
+
 import { createApiClient } from "$lib/server/api-client";
 import type { PageServerLoad } from "./$types";
+import { getconfigData } from "$lib/server/config-data";
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
+    const configData = await getconfigData();
     const api = createApiClient(locals.sessionCookie);
     const timelines = await api.core.coreApiListTimelines({
         published: true,
     });
-
     const accommodationsResponse = await api.core.coreApiListAccommodations({});
+
+    if (!configData?.enableVenue) {
+        throw redirect(302, "/");
+    }
 
     return {
         timelines: timelines.items || [],
